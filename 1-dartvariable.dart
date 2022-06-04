9f61db5831efa915940ce3b358cf7917bc299762
EFTEKHER <eftekherali2000@gmail.com>
2022-06-05 04:39:19 +0600

    dart day1

---
+++ 1-dartvariable.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  int age=20;
+  double a=20.9;
+  String name="Arfan";
+  bool value=false;
+  List mylist=[1,5,67,89,90];
+  Set myset={'one','two','three','four'};
+Map<String,dynamic>myMap={
+'name':'Eftekher Ali Efte',
+'age':21
+
+};
+
+print(myMap);
+var b="Efte";
+print(b);
+}
