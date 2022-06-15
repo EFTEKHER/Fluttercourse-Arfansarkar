f651917158947a65e435c3d7fcc4e54e5b96d2eb
EFTEKHER <eftekherali2000@gmail.com>
2022-06-15 22:05:52 +0600

    map

---
+++ DartDay1/conditional-expression.dart
@@ -0,0 +1,14 @@
+void main() {
+// int a=10;// var result= a is int;
+// print(result);
+// String x="10";
+// var result2=x is int;
+// print(result2);
+// var result3=x is! int;
+// print(result3);
+var num1 =10;
+var result= num1==10?"equals to Ten":"not equals to Ten";
+print(result);
+var a;
+print(a.runtimeType);
+}
---
+++ DartDay1/map-list.dart
@@ -0,0 +1,22 @@
+void main(){
+
+var num2=10;
+double numTwo = 3.4556;
+print(numTwo);
+String name = "Efte";
+bool value = false;
+List<int> myList = [1, 2, 3, 4, 5, 6];
+
+List<Map> mymap1 = [
+{"name": "Efte"},
+{"name": "kher"},
+{"name": "Ali"},
+];
+
+var mymap2=[
+{"name": "Efte"},
+{"name": "kher"},
+{"name": "Ali"},
+];
+print(num2.runtimeType);
+}
