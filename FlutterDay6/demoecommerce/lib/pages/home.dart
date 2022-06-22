d0784607e9fd587e2c7ec7253a5c8e92328ceb0a
EFTEKHER <eftekherali2000@gmail.com>
2022-06-23 02:28:26 +0600

    y

--- FlutterDay6/demoecommerce/lib/pages/home.dart
+++ FlutterDay6/demoecommerce/lib/pages/home.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  
+/*
+create 
+1)coumn
+2)Text
+3)Text
+4)padding
 
+*/
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
-    );
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(top: 30, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hello  Efte",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              "Lets gets something?",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF4F4C4C)),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 150,
+              width: 300,
+              //  color:Colors.amber,
+              //you have to set color in decoration
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                //you have to set color in decoration
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "40% Off During\n Covid 19",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Image.asset(
+                    "image.png",
+                    width: 100,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
