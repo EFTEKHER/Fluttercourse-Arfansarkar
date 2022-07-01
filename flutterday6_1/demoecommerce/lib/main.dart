54d7d106332704f598c76c0669392dbd3e4cc0eb
EFTEKHER <eftekherali2000@gmail.com>
2022-07-02 02:39:01 +0600

    yes

--- flutterday6_1/demoecommerce/lib/main.dart
+++ flutterday6_1/demoecommerce/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:demoecommerce/bottom_nav_controller.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+ 
 
   
   @override
--- flutterday6_1/demoecommerce/lib/pages/my_ads.dart
+++ flutterday6_1/demoecommerce/lib/pages/my_ads.dart
@@ -1,60 +1,127 @@
-import 'package:demoecommerce/pages/my_ads/My_Ads_one.dart';
-import 'package:demoecommerce/pages/my_ads/My_Favourites_two.dart';
+import 'package:demoecommerce/pages/details_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyAdsScreen extends StatefulWidget  {
-  
-
+class MyAdsScreen extends StatefulWidget {
   @override
   State<MyAdsScreen> createState() => _MyAdsScreenState();
 }
 
-class _MyAdsScreenState extends State<MyAdsScreen> with TickerProviderStateMixin {
-  late TabController _tabController;
+class _MyAdsScreenState extends State<MyAdsScreen> {
+  final _formkey=GlobalKey<FormState>();
+
+  late var _passwordvisible=true;
+  TextEditingController _controller=TextEditingController();
   @override
-  void initState() {
-    _tabController=TabController(length: 2, vsync: this);
-    super.initState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        print(_controller.text);
+      },child: Icon(Icons.accessible),),
+      body: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Form(
+                key: _formkey,
+                autovalidateMode: AutovalidateMode.always,
+                child: Column(
+                  children: [
+
+                    //input from user
+                    // TextField(),
+                    //validation
+                    SizedBox(
+                      height: 100,
+                    ),
+                    TextField(
+                      controller: _controller,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(hintText: "Enter your email :")
+                      ,
+                      validator: (val){
+                        if(val!.isEmpty)
+                        {
+                          return "Value cannot be empty";
+                        }
+                        else if(val.length<4)
+                        {
+                          return "invalid email";
+                        }
+                        else 
+                        {
+                          return null;
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(hintText: "Enter your name :"),
+                      validator: (val){
+                        if(val!.isEmpty)
+                        {
+                          return "Value cannot be empty";
+                        }
+                      
+                        else 
+                        {
+                          return null;
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: _passwordvisible,
+                      keyboardType: TextInputType.text,
+                      decoration:InputDecoration(hintText: "Enter your password :"
+                      , suffixIcon: IconButton(
+            icon: Icon(
+              // Based on passwordVisible state choose the icon
+               _passwordvisible
+               ? Icons.visibility
+               : Icons.visibility_off,
+               color: Theme.of(context).primaryColorDark,
+               ),
+            onPressed: () {
+               // Update the state i.e. toogle the state of passwordVisible variable
+               setState(() {
+                 _passwordvisible=!_passwordvisible;
+               });
+             },
+            ),
+                      ),
+                          validator: (val){
+                        if(val!.isEmpty)
+                        {
+                          return "Value cannot be empty";
+                        }
+                        else if(val.length<6)
+                        {
+                          return "password length must be 6 digits";
+                        }
+                        else 
+                        {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+              
+                    SizedBox(
+                      width: 400,
+                      child: ElevatedButton(onPressed: () {
+                       if(_formkey.currentState!.validate())
+                       {
+                          Navigator.push(context, CupertinoPageRoute(builder:(context) => detailsScreen(),
+                          )
+                          );
+                       }
+                      }, child: Text('Validate now'))),
+                  ],
+                ),
+              ))),
+    );
   }
-  @override
-  Widget build(BuildContext context){
-  return Scaffold(
-appBar: AppBar(
-backgroundColor: Colors.white,
-centerTitle: true,
-          title: Text(
-            'MY ADS',
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-          ),
-bottom: TabBar( controller: _tabController ,tabs: const[
-
-   Tab(
-                  child: Text(
-                    'My Ads',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  icon: Icon(
-                    Icons.shopping_bag_outlined,
-                    color: Colors.black,
-                  )),
-              Tab(
-                  child: Text(
-                    'My Favourites',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  icon: Icon(
-                    Icons.favorite_border,
-                    color: Colors.black,
-                  )),
-]),
-
-),
-body:TabBarView( controller: _tabController ,children: [
-MyAdsOne(),
-MyFavouriteTwo(),
-
-])
-
-  );
-
-  }}
+}
